c28233caadf49fe5e58056131bbe8119a1bb68c4
Brian Peterson <bepetersn@gmail.com>
2020-09-14 06:11:48 -0600

    Add missing '\n's. Flush buffer, make last print work

--- cs3753_add.c
+++ cs3753_add.c
@@ -7,13 +7,13 @@ asmlinkage long sys_cs3753_add(int a, int b, int *dest)
 {
     int sum;
     // use printk() to log the numbers to be added
-    printk("Going to add %d and %d", a, b);
+    printk("Going to add %d and %d\n", a, b);
     // add those two numbers 
     sum = a + b;
     // store the result
     put_user(sum, dest);
     // use printk() to log the result
-    printk("Sum: %d", sum);
+    printk("Sum: %d\n", sum);
     // return an appropriate return value
     return 0;
 }
